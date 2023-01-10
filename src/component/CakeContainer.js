4fc6bce25bc9898271e33da92d0d794e4e6377f1
RadimYassin <100171335+RadimYassin@users.noreply.github.com>
2023-01-10 21:33:43 +0100

    add mapStateToProps ad mapDispatchToProps

--- src/component/CakeContainer.js
+++ src/component/CakeContainer.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import {buyCake} from '../redux'
 
-function CakeContainer() {
+function CakeContainer(props) {
   return (
     <div>
-      <h2 >number of cke </h2>
-      <button>nay Cake </button>
+      <h2 >number of cake : {props.num} </h2>
+      <button onClick={props.buyCake}>bay Cake </button>
     </div>
   )
 }
 
-export default CakeContainer
+
+const mapStateToProps=state=>{
+  return{
+    num:state.num 
+  }
+}
+const mapDispatchToProps=dispatch=>{
+  return {
+    buyCake:()=>dispatch(buyCake())
+  }
+}
+
+export default connect( mapStateToProps,mapDispatchToProps) (CakeContainer)
+
